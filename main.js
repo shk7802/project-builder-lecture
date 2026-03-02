864ec69a9db90a0bc48a5d157effa3f3e290acc2
shk7802 <shk7802@gmail.com>
2026-03-02 09:45:53 +0000

    Change lotto generator to 5 numbers

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const generateLottoNumbers = () => {
         const numbers = new Set();
-        while (numbers.size < 6) {
+        while (numbers.size < 5) {
             const randomNumber = Math.floor(Math.random() * 45) + 1;
             numbers.add(randomNumber);
         }
