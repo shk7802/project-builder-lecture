fe5accd808f44852961700f6860c6157c8b856aa
shk7802 <shk7802@gmail.com>
2026-04-10 06:04:26 +0000

    feat: 제휴 문의 폼 추가 및 스타일 업데이트

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// Add JS here
+const generateBtn = document.getElementById('generate');
+const numberSpans = document.querySelectorAll('.number');
+
+generateBtn.addEventListener('click', () => {
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        numbers.add(randomNumber);
+    }
+
+    const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+
+    numberSpans.forEach((span, index) => {
+        span.textContent = sortedNumbers[index];
+    });
+});
